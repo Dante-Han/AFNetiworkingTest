5234558854f9ea70c308f295cede7492bc127b4a
SangminHan <yeehaw@live.co.kr>
2014-02-07 17:43:18 +0900

    0.4.1 EarGray
    NetWork모델링 작업

--- AFNetiworkingTest/ViewController.m
+++ AFNetiworkingTest/ViewController.m
@@ -10,9 +10,18 @@
 #import <AFNetworking.h>
 #import <UIImageView+AFNetworking.h>
 
-#define URL @"http://petmily_2.0164740237.cloulu.com/fampage"
+#define URL @"http://pettest.0164740237.cloulu.com/myhome"
 @interface ViewController ()
-@property (strong, nonatomic) IBOutlet UIView *imageView;
+
+
+@property (readwrite,nonatomic,copy)    NSMutableDictionary *data;
+@property (readwrite,nonatomic,copy) NSArray *array;
+@property (readwrite,nonatomic,copy)    NSString *string;
+
+
+@property (weak, nonatomic) IBOutlet UIImageView *imageView;
+
+
 
 @end
 
@@ -22,15 +31,16 @@
 {
     [super viewDidLoad];
 
-
     AFHTTPRequestOperationManager *manager = [AFHTTPRequestOperationManager manager
                                               ];
 
-    NSDictionary *parameters = @{@"fam_id":@"aaa"};
+    NSDictionary *parameters = @{@"fam_id":@"ggg"};
+    self.data = [[NSMutableDictionary alloc]init];
 
     [manager POST:URL parameters:parameters success:^(AFHTTPRequestOperation *operation, id responseObject) {
-//        [NSLog(@"json: %@",[operation responseString]);
-         id innerJSON = [responseObject objectForKey:@"5"];
+//        data=[operation responseString];
+    //    NSLog(@"%@",responseObject);
+        self.data = [responseObject objectForKey:@"fampage"][0];
 
 
     } failure:
@@ -38,9 +48,32 @@
          NSLog(@"Error:%@",error);
      }];
 
+//    item=[[NSArray alloc]init];
 
-    [self.uiimagView setImageWithURL:[NSURL URLWithString:URL]];
+/*    NSString *labelstring= [NSString stringWithFormat:@"%@",[dic objectForKey:@"user_name"]];
+    NSLog(@"%@",[dic objectForKey:@"user_name"]);*/
+  //  self.label.text = labelstring;
+
     // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (IBAction)ok:(id)sender
+{
+    NSLog(@"data:%@",self.data);
+             NSLog(@"post_no :%@", [self.data objectForKey:@"post_no"]);
+
+
+    NSURL *url = [NSURL URLWithString:[self.data objectForKey:@"post_img"]];
+
+
+    NSString *string = [NSString stringWithFormat:@"%@",[self.data objectForKey:@"fam_name"]];
+    NSLog(@"%@",string);
+
+    [self.imageView setImageWithURL:url];
+
+    self.label.text = string;
+
+
 }
 
 - (void)didReceiveMemoryWarning
